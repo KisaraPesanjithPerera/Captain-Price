44ca1a1f5d8d629d558064dbb5da2a9811b9568a
Senu Gamer Boy <86920820+SenuGamerBoy@users.noreply.github.com>
2021-09-03 20:01:00 +0530

    Bug Fixed 😆

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -273,7 +273,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "about":
             buttons = [
                 [
-                    InlineKeyboardButton('Update Channel', url='t.me/Mo_Tech_YT'),
+                    InlineKeyboardButton('Update Channel', url='t.me/senuinfinity'),
                     InlineKeyboardButton('Source Code', url=f'{TUTORIAL}')
                 ]
                 ]
