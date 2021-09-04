ae532e2d0b8af77331ee6994e5d0137503df16c2
Senu Gamer Boy <86920820+SenuGamerBoy@users.noreply.github.com>
2021-09-04 10:58:27 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+            InlineKeyboardButton('🖥️ Support 🖥️', url=f'{TUTORIAL}')
         ],
         [
             InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
