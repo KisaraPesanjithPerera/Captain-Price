8a1a8b100ae35b52b818e855d85060b87a2620d9
Senu Gamer Boy <86920820+SenuGamerBoy@users.noreply.github.com>
2021-09-10 18:12:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -32,10 +32,10 @@ async def start(bot, cmd):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("♨ Join Updates Channel ♨", url=invite_link.invite_link)
+                                InlineKeyboardButton("🗱 Join Updates Channel 🗱", url=invite_link.invite_link)
                             ],
                             [
-                                InlineKeyboardButton("♻ Try Again ♻", callback_data=f"checksub#{file_id}")
+                                InlineKeyboardButton("↺ Try Again ↻", callback_data=f"checksub#{file_id}")
                             ]
                         ]
                     ),
