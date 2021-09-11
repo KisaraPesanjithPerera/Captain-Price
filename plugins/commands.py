b00e13c049e0c3f5c843518d1bcc989c6394850c
Senu Gamer Boy <86920820+SenuGamerBoy@users.noreply.github.com>
2021-09-11 16:45:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -199,4 +199,4 @@ async def bot_info(bot, message):
             InlineKeyboardButton('Video', url=f'{TUTORIAL}')
         ]
         ]
-    await message.reply(text=f"<b>Developer : <a href='https://t.me/senuinfinity'>Senu Infinity</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram</a>\nSupport : <a href='{TUTORIAL}'>Click here</a>\nUpdate Channel : <a href='https://t.me/senuinfinitygroup'>Support Group</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text=f"<b>👋Hi,\nI am Very Powerfull Movie Filter Bot By @senuinfinity\n\nAbout Me 🗯\n🔬Language : <code>Python3</code>\n📚Library : <a href='https://docs.pyrogram.org/'>Pyrogram</a>\nSource Code 📑 : 🔐\nSupport 🌻: <a href='{TUTORIAL}'>Click here</a>\nMy Updates 🎇: <a href='https://t.me/senuinfinitygroup'>Click here</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
