b781728b700944a90630b46161f30467f0e4df60
KisaraPesanjithPerera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-09-25 19:05:04 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('☀ Support ☀', url=f'{TUTORIAL}')
+            InlineKeyboardButton('🖲️ Updates 🖲️', url='t.me/MHO_Alert')
         ],
         [
             InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
