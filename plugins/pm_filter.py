65534815c3318a324973391efafea1246780bd94
KisaraPesanjithPerera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-11-01 04:23:40 -0700

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -70,7 +70,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("Please do not view other requests 😢. You request again 🤗", show_alert=True)
     try:
         offset = int(offset)
     except:
