b15e62a23f1bb777c158822add0839ad84adcf6e
KisaraPesanjithPerera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-11-01 01:11:30 -0700

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,3 +1,4 @@
+import os
 from info import AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, API_KEY, AUTH_GROUPS, BUTTON,  IMDB
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
