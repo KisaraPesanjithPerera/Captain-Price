9cd750f72af95c27ade4a36a87684140a13aac28
KisaraPesanjithPerera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-11-01 01:07:59 -0700

    Update info.py

--- info.py
+++ info.py
@@ -34,6 +34,7 @@ Here you can search files in Inline mode as well as PM, Use the below buttons to
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 
+IMDB = eval((environ.get('IMDB', "True")))
 BUTTON = environ.get("BUTTON",False)
 FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
 OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
