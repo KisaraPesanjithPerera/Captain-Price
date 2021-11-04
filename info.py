b1d74e6a758180df01d3640f0c42d7c944380b99
Senu Gamer Boy 〽 <86920820+SenuGamerBoy@users.noreply.github.com>
2021-11-04 21:39:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,13 @@ import re
 from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
