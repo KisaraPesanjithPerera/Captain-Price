232f4e0193ba742c211ee54999c4804b720b540a
Senu Gamer Boy 〽 <86920820+SenuGamerBoy@users.noreply.github.com>
2021-11-05 11:54:25 -0700

    Delete tutorial.py

--- plugins/tutorial.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.types import Message
-
-@Client.on_message(filters.command("How"))
-async def howto(client, message):
-      client.send_photo(massage.chat.id, "https://www.creative-tim.com/blog/content/images/2021/07/coming-soon.jpg" caption="Coming Soon 👏👏👏")
