56174ece3359dbbf263915cf9e50400f5855e363
Kisara Pesanjith Perera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-11-13 08:02:23 -0700

    Update Script.py

--- Script.py
+++ Script.py
@@ -40,12 +40,17 @@ Jelly Bean is a movie auto filter bot as well as one bot in Telegram that can do
     EXTRAMOD_TXT = """Help: <b>Extra Modules 🍀</b>
     
 <b>NOTE:</b>
+
 these are the extra features of 🔥𝐌𝐨𝐯𝐢𝐞𝐇𝐮𝐛 𝐅𝐢𝐥𝐭𝐞𝐫𝐬🔥
+
 <b>Commands and Usage:</b>
+
 • /id - <code>get id of a specifed user.</code>
 • /info  - <code>get information about a user.</code>
 • /imdb  - <code>get the film information from IMDb source.</code>
+
 Coming Soon features For 🔥𝐌𝐨𝐯𝐢𝐞𝐇𝐮𝐛 𝐅𝐢𝐥𝐭𝐞𝐫𝐬🔥
+
 Movie Player , Movie Uploader"""
     
     ADMIN_TXT = """
@@ -57,10 +62,13 @@ STATUS_TXT = """My Status 🤗
 📝 Total Groups : <code>{}</code>
 🔰 Used Space : <code>{}</code> MB
 🗃️ Free Space : <code>{}</code> MB
+
 My Server Status 🍀
+
 💻 CPU Usage :</b> {}%
 ☄️ RAM Usage :</b> {}%"""
-    LOG_TEXT_G = """#NewGroup
+    LOG_TEXT_G = """
+#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
 Added By - {}
@@ -75,6 +83,7 @@ MANUELFILTER_TXT = """ Coming Soon 🌺"""
 AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
     
 <b>NOTE:</b>
+
 1. Add me to your group
 2. Then give me admin
 3. Just send the name of the movie/TV series.🎥 Result will come to you.😍"""
@@ -82,10 +91,14 @@ CONNECTION_TXT = """Help: <b>Connections</b>
     
 - Used to connect bot to PM for managing filters 
 - it helps to avoid spamming in groups.
+
 <b>NOTE:</b>
+
 1. Only admins can add a connection.
 2. Send <code>/connect</code> for connecting me to ur PM
+
 <b>Commands and Usage:</b>
+
 • /connect  - <code>connect a particular chat to your PM</code>
 • /disconnect  - <code>disconnect from a chat</code>
 • /connections - <code>list all your connections</code>"""
