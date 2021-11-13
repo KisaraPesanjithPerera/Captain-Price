b92f352b839d0e2323e8171f78037272f1e41abb
Kisara Pesanjith Perera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-11-13 07:25:20 -0700

    Update Script.py

--- Script.py
+++ Script.py
@@ -22,6 +22,21 @@ class script(object):
 📡 <b>Server :</b> [heroku](https://heroku.com)
 🌹 <b>Build Status :</b> 𝚅2.5
 """
+    credits = """🔘 Just Send Series Name and Season OR Use Inline Search Button.
+    
+[Dont Use words Like Season/Episode/Series](https://t.me/MovieHubOfficialSL)
+
+♻️ If any series is Not Available, Then Request it at
+@MovieHubOfficialSL
+
+✨ <b>My Name :</b>  [🔥𝐌𝐨𝐯𝐢𝐞𝐇𝐮𝐛 𝐅𝐢𝐥𝐭𝐞𝐫𝐬🔥](t.me/MHOFilter_bot)
+👨‍💻 <b>Developer :</b> [𝚂𝚎𝚗𝚞 𝙶𝚊𝚖𝚎𝚛 𝙱𝚘𝚢](https://github.com/SenuGamerBoy)
+🍀 <b>Data Base :</b> Mango DB
+📝 <b>Language :</b> [Python3](https://python.org)
+🧰 <b>Framework :</b> [Pyrogram](https://pyrogram.org)
+📡 <b>Server :</b> [heroku](https://heroku.com)
+🌹 <b>Build Status :</b> 𝚅2.5
+"""
     SOURCE_TXT = """MMa</a>"""
     LOGO_TEXT = """ 🔹 <code>/hlogo</code> <text> :  Create your logo with your name
  🔹 <code>/alogo</code> <text> :  Create your logo with your name"""
