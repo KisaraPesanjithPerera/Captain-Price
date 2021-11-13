bc5388071d4caf11e47cc56c44974d8c7e88eae1
Senu Gamer Boy 〽 <86920820+SenuGamerBoy@users.noreply.github.com>
2021-11-13 01:37:07 -0700

    Update tutorial.py

--- plugins/tutorial.py
+++ plugins/tutorial.py
@@ -1,8 +1,25 @@
+
 import pyrogram
 import asyncio
 import os
 from pyrogram import Client, filters
 
-@Client.on_message(filters.privet & filters.video)
-def tvidei(bot, message):
-  await message.reply(message.video.file_id)
+
+tutorial1 = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Next ➡️" callback_data="helpmenu"),
+                    InlineKeyboardButton("Close", callback_data="close")
+                ]
+            ]
+        )
+
+tutorial1t = "Hello {}. \n Coming Soon Tutorial "
+
+@Client.on_message(filters.command("tutorial"))
+async def tutorial1(bot, message):
+    await update.reply_video(
+                    video="https://telegra.ph/file/fe9fcc029fd846457fac7.mp4",
+                    reply_markup = tutorial1,
+                    caption = tutorial1t.format(update.from_user.mention))
+    
