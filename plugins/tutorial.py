723f37ba6dd7caabf05d5576a3b6a993e87242c4
Senu Gamer Boy 〽 <86920820+SenuGamerBoy@users.noreply.github.com>
2021-11-13 01:15:54 -0700

    Update tutorial.py

--- plugins/tutorial.py
+++ plugins/tutorial.py
@@ -1,1 +1,8 @@
+import pyrogram
+import asyncio
 import os
+from pyrogram import Client, filters
+
+@bot.on_message(filters.privet & filters.video)
+async def tvidei(bot, message):
+  await message.reply(message.video.file_id)
