6c790274a389c66e90e3d0ddec114d9a237683b2
Senu Gamer Boy 〽 <shamila2891@gmail.com>
2021-11-21 06:27:48 -0700

    Create cust_p_filters.py

---
+++ helpers/helper_functions/cust_p_filters.py
@@ -0,0 +1,48 @@
+from pyrogram import (
+    filters
+)
+from pyrobot import (
+    SUDO_USERS,
+    USE_AS_BOT
+)
+from pyrobot.helper_functions.admin_check import admin_check
+
+
+def f_sudo_filter(filt, client, message):
+    return bool(
+        message.from_user.id in SUDO_USERS
+    )
+
+
+sudo_filter = filters.create(
+    func=f_sudo_filter,
+    name="SudoFilter"
+)
+
+
+def onw_filter(filt, client, message):
+    if USE_AS_BOT:
+        return bool(
+            True # message.from_user.id in SUDO_USERS
+        )
+    else:
+        return bool(
+            message.from_user and
+            message.from_user.is_self
+        )
+
+
+f_onw_fliter = filters.create(
+    func=onw_filter,
+    name="OnwFilter"
+)
+
+
+async def admin_filter_f(filt, client, message):
+    return await admin_check(message)
+
+
+admin_fliter = filters.create(
+    func=admin_filter_f,
+    name="AdminFilter"
+)
