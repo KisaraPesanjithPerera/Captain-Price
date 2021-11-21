13207ff3a8262dda7a54201738371a542f8f0440
Senu Gamer Boy 〽 <shamila2891@gmail.com>
2021-11-21 06:37:31 -0700

    Update pin.py

--- plugins/pin.py
+++ plugins/pin.py
@@ -6,14 +6,14 @@ from helpers.helper_functions.cust_p_filters import (
     admin_fliter
 )
 
-@Client.on_message(filters.command("pin"))
+@Client.on_message(filters.command("pin") & admin_fliter)
 async def pin(_, message: Message):
     if not message.reply_to_message:
         return
     await message.reply_to_message.pin()
 
 
-@Client.on_message(filters.command("unpin"))
+@Client.on_message(filters.command("unpin") & admin_fliter)
 async def unpin(_, message: Message):
     if not message.reply_to_message:
         return
