9fef291211da00a98e190ac5e3db4f0ef5f2390c
Kisara Pesanjith Perera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-11-29 01:02:26 -0700

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -13,13 +13,6 @@ def map_btns(pos):
     if pos == 1:
         button = [[InlineKeyboardButton(text="-->", callback_data="help+2")]]
     elif pos == len(tr.HELP_MSG) - 1:
-        auth = GoogleAuth(Config.CLIENT_ID, Config.CLIENT_SECRET)
-        url = auth.GetAuthUrl()
-        button = [
-            [InlineKeyboardButton(text="<--", callback_data=f"help+{pos-1}")],
-            [InlineKeyboardButton(text="Authentication URL", url=url)],
-        ]
-    else:
         button = [
             [
                 InlineKeyboardButton(text="<--", callback_data=f"help+{pos-1}"),
